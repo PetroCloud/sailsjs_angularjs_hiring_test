6dbdc7a3510c68ceba9fd1dbba56faabb2fd37e4
odruslan <odruslan@gmail.com>
2018-07-18 19:24:39 +0300

    init commit

---
+++ api/models/Dataset.js
@@ -0,0 +1,18 @@
+/**
+ * Dataset.js
+ *
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    id: {
+      type: 'integer'
+    },
+    json: {
+      type: 'longtext'
+    }
+  }
+};
+
